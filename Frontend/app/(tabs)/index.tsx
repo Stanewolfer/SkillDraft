0d40ae09daed10349ec204ace54e355c058a1240
ScorpioOffi <117345081+ScorpioOffi@users.noreply.github.com>
2025-03-04 11:49:20 +0100

    change index page to auth

--- Frontend/app/(tabs)/index.tsx
+++ Frontend/app/(tabs)/index.tsx
@@ -1,74 +1,100 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React from 'react';
+import { LogBox, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import * as Unicons from '@iconscout/react-native-unicons';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+console.log("Icônes disponibles:", Object.keys(Unicons));
+
+LogBox.ignoreLogs([
+  "Warning: UilSignin: Support for defaultProps will be removed from function components",
+  "Warning: UilUser: Support for defaultProps will be removed from function components"
+]);
+
+const IconUser = Unicons.UilSignin;
+const IconSignIn = Unicons.UilUser;
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Bienvenue sur Skilldraft !</Text>
+        <View style={styles.titleUnderline} />
+      </View>
+
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity style={styles.card}>
+          <Text style={styles.cardText}>Inscription</Text>
+          {IconUser ? (
+            <IconUser size={100} color="#010017" />
+          ) : (
+            <Text style={{ color: 'red' }}>Icon User introuvable</Text>
+          )}
+        </TouchableOpacity>
+
+        <TouchableOpacity style={[styles.card, styles.cardInverted]}>
+          {IconSignIn ? (
+            <IconSignIn size={100} color="#A9F6CB" />
+          ) : (
+            <Text style={{ color: 'red' }}>Icon SignIn introuvable</Text>
+          )}
+          <Text style={[styles.cardText, styles.cardTextInverted]}>Connexion</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
+    backgroundColor: '#010017',
     alignItems: 'center',
-    gap: 8,
+    paddingHorizontal: 20,
+    paddingTop: 150,
   },
-  stepContainer: {
-    gap: 8,
+  titleContainer: {
+    alignItems: 'center',
+    marginBottom: 40,
+  },
+  title: {
+    fontSize: 20,
+    color: '#FFFFFF',
+    fontFamily: 'Montserrat',
+    fontWeight: 'bold',
+  },
+  titleUnderline: {
+    marginTop: 4,
+    height: 3,
+    width: 150,
+    backgroundColor: '#A9F6CB',
+    borderRadius: 2,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    paddingTop: 100,
+  },
+  card: {
+    flex: 1,
+    marginHorizontal: 10,
+    height: 150,
+    backgroundColor: '#A9F6CB',
+    borderRadius: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cardText: {
+    color: '#010017',
+    fontSize: 16,
+    fontWeight: '600',
     marginBottom: 8,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  cardInverted: {
+    backgroundColor: '#010017',
+    borderWidth: 5,
+    borderColor: '#A9F6CB',
+  },
+  cardTextInverted: {
+    color: '#A9F6CB',
   },
 });
