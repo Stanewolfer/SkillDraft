d80d25edda33cca5bdf462ae2b654a2151d91159
Sacha <sacha.harel@gmail.com>
2025-03-06 17:43:35 +0100

    UPDATE // Create post commit

--- Frontend/app/(tabs)/create_post.tsx
+++ Frontend/app/(tabs)/create_post.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { COLORS } from './styles/colors';
+import { Stack } from 'expo-router'
+import React, { useState } from 'react'
+import { COLORS } from './styles/colors'
+import {
+  ScrollView,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput
+} from 'react-native'
+import CustomStackScreen from '../components/CustomStackScreen'
+import { useRouter } from 'expo-router'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-const CreatePost = () => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Create Post</Text>
-        </View>
-    );
-};
+export default function ConnexionScreen() {
+  const router = useRouter()
+
+  // const [mode, setMode] = useState<Mode>(Mode.Personne)
+
+  return (
+    <>
+        <CustomStackScreen title="Créer un post" />
+        <ScrollView contentContainerStyle={styles.container}>
+            <TextInput
+                placeholder="Titre"
+                style={ styles.inputWrapper }
+                placeholderTextColor={COLORS.main_blue}
+            />
+            <TextInput
+                placeholder="Contenu"
+                multiline
+                style={ styles.inputWrapper }
+                placeholderTextColor={COLORS.main_blue}
+            />
+            <TouchableOpacity
+                onPress={() => {
+                    // Handle post creation logic here
+                }}
+                style={{ backgroundColor: COLORS.background_blue, padding: 10, alignItems: 'center' }}
+            >
+                <Text style={{ color: '#fff' }}>Créer</Text>
+            </TouchableOpacity>
+        </ScrollView>
+    </>
+  )
+}
 
 const styles = StyleSheet.create({
     container: {
+        backgroundColor: COLORS.background_blue,
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: COLORS.text_white,
     },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
+    
+    inputWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: COLORS.main_blue,
+        color: COLORS.main_blue,
+        borderRadius: 25,
+        paddingVertical: 8,
+        paddingHorizontal: 12,
+        marginBottom: 12
     },
-});
-
-export default CreatePost;
+})
