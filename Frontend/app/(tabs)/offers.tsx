6dcf389d80417d18458308cc0fb149f2046039ff
ScorpioOffi <117345081+ScorpioOffi@users.noreply.github.com>
2025-04-07 15:09:02 +0200

    start offers page

--- Frontend/app/(tabs)/offers.tsx
+++ Frontend/app/(tabs)/offers.tsx
@@ -1,26 +1,152 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { useRouter } from 'expo-router'
+import {
+  ScrollView,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  LogBox
+} from 'react-native'
+import * as Unicons from '@iconscout/react-native-unicons'
 import { COLORS } from './styles/colors'
+import CustomStackScreen from '../components/CustomStackScreen'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-const Messaging = () => {
+LogBox.ignoreLogs([
+  'Warning: UilHeart: Support for defaultProps will be removed',
+  'Warning: UilCommentAlt: Support for defaultProps will be removed',
+  'Warning: UilRepeat: Support for defaultProps will be removed',
+  'Warning: UilCornerUpRight: Support for defaultProps will be removed',
+  'Warning: UilAirplay: Support for defaultProps will be removed',
+  'Warning: UilPlusCircle: Support for defaultProps will be removed',
+  'Warning: UilBag: Support for defaultProps will be removed',
+  'Warning: UilEnvelopeAlt: Support for defaultProps will be removed',
+  'Warning: UilSignout: Support for defaultProps will be removed'
+])
+
+export default function Offers() {
+  const router = useRouter()
+
+  const logout = async () => {
+    await AsyncStorage.clear()
+    router.push('/')
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Offers</Text>
-    </View>
+    <>
+      <CustomStackScreen title='Feed' />
+      <ScrollView contentContainerStyle={styles.container}>
+
+
+
+        {/* Barre de navigation en bas */}
+        <View style={styles.bottomButtonsContainer}>
+          <TouchableOpacity
+            style={styles.bottomButton}
+            //onPress={() => router.push('/news_feed')}
+          >
+            <View style={styles.bottomButtonContent}>
+              <Unicons.UilNewspaper size={28} color={COLORS.main_blue} />
+              <Text style={styles.bottomButtonLabel}>Fil d'actualité</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.bottomButton}
+            //onPress={() => router.push('/quick_search')}
+          >
+            <View style={styles.bottomButtonContent}>
+              <Unicons.UilSearch size={28} color={COLORS.main_blue} />
+              <Text style={styles.bottomButtonLabel}>Recherches rapides</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.bottomButton}
+            onPress={() => router.push('/create_post')}
+          >
+            <View style={styles.bottomButtonContent}>
+              <Unicons.UilPlusCircle size={28} color={COLORS.main_blue} />
+              <Text style={styles.bottomButtonLabel}>Ajouter un post</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.bottomButton, styles.activeBottomButton]}
+            onPress={() => router.push('/offers')}
+          >
+            <View style={styles.bottomButtonContent}>
+              <Unicons.UilBag size={28} color={COLORS.background_blue} />
+              <Text style={[styles.bottomButtonLabel, styles.activeBottomButtonLabel]}>
+                Offres
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.bottomButton}
+            onPress={() => router.push('/messaging')}
+          >
+            <View style={styles.bottomButtonContent}>
+              <Unicons.UilEnvelopeAlt size={28} color={COLORS.main_blue} />
+              <Text style={styles.bottomButtonLabel}>Messagerie</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.bottomButton} onPress={logout}>
+            <View style={styles.bottomButtonContent}>
+              <Unicons.UilSignout size={28} color={COLORS.main_blue} />
+              <Text style={styles.bottomButtonLabel}>Déconnexion</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: COLORS.text_white
+    padding: 10,
+    backgroundColor: COLORS.background_blue
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold'
+  // --- Barre de navigation en bas ---
+  bottomButtonsContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: COLORS.background_blue,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 0,
+    margin: 0,
+    overflow: 'hidden',
+    borderTopWidth: 1,
+    borderColor: COLORS.main_blue,
+    //borderTopLeftRadius: 35,
+    //borderTopRightRadius: 35,
+    zIndex: 999
+  },
+  bottomButton: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 0,
+    padding: 0
+  },
+  bottomButtonContent: {
+    alignItems: 'center'
+  },
+  bottomButtonLabel: {
+    marginTop: 4,
+    fontSize: 12,
+    color: COLORS.main_blue
+  },
+  activeBottomButton: {
+    backgroundColor: COLORS.main_blue,
+    paddingVertical: 10,
+    borderRadius: 8
+  },
+  activeBottomButtonLabel: {
+    color: COLORS.background_blue
   }
 })
-
-export default Messaging
