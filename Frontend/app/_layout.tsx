872b1ec8c3109ca055d295466fa1a731c45a8e10
Sacha <sacha.harel@gmail.com>
2025-06-12 14:45:10 +0200

    UPDATES // Added fonts

--- Frontend/app/_layout.tsx
+++ Frontend/app/_layout.tsx
@@ -37,7 +37,27 @@ export default function RootLayout() {
 
   const colorScheme = useColorScheme()
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf')
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    Montserrat: require('../assets/fonts/Montserrat-Regular.ttf'),
+    MonserratBlack: require('../assets/fonts/Montserrat-Black.ttf'),
+    MontserratBlackItalic: require('../assets/fonts/Montserrat-BlackItalic.ttf'),
+    MontserratBold: require('../assets/fonts/Montserrat-Bold.ttf'),
+    MontserratBoldItalic: require('../assets/fonts/Montserrat-BoldItalic.ttf'),
+    MontserratExtraBold: require('../assets/fonts/Montserrat-ExtraBold.ttf'),
+    MontserratExtraBoldItalic: require('../assets/fonts/Montserrat-ExtraBoldItalic.ttf'),
+    MontserratExtraLight: require('../assets/fonts/Montserrat-ExtraLight.ttf'),
+    MontserratExtraLightItalic: require('../assets/fonts/Montserrat-ExtraLightItalic.ttf'),
+    MontserratItalic: require('../assets/fonts/Montserrat-Italic.ttf'),
+    MontserratLight: require('../assets/fonts/Montserrat-Light.ttf'),
+    MontserratLightItalic: require('../assets/fonts/Montserrat-LightItalic.ttf'),
+    MontserratMedium: require('../assets/fonts/Montserrat-Medium.ttf'),
+    MontserratMediumItalic: require('../assets/fonts/Montserrat-MediumItalic.ttf'),
+    MontserratRegular: require('../assets/fonts/Montserrat-Regular.ttf'),
+    MontserratSemiBold: require('../assets/fonts/Montserrat-SemiBold.ttf'),
+    MontserratSemiBoldItalic: require('../assets/fonts/Montserrat-SemiBoldItalic.ttf'),
+    MontserratThin: require('../assets/fonts/Montserrat-Thin.ttf'),
+    MontserratThinItalic: require('../assets/fonts/Montserrat-ThinItalic.ttf')
+
   })
 
   useEffect(() => {
