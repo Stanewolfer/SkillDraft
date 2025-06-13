25671ad419d76df2eb7414ad359be82d24866cbf
Sacha <sacha.harel@gmail.com>
2025-06-13 05:07:01 +0200

    update auth.controller.ts, fileUpload.controller.ts, posts.controller.ts and .env

--- Backend/src/controllers/fileUpload.controller.ts
+++ Backend/src/controllers/fileUpload.controller.ts
@@ -6,7 +6,7 @@ export const uploadFile = (req: Request) => {
   }
 
   // Exemple : retourne juste l'URL de l'image
-  const fileUrl = `${req.protocol}://localhost:5000/uploads/${req.file.filename}`
+  const fileUrl = `${req.protocol}://127.0.0.1:4040/uploads/${req.file.filename}`
 
   return {
     filename: req.file.filename,
